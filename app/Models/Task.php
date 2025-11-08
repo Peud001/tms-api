4c966c4214dff236d416bad954ad93edbd85a0b9
Solomon Udofia <meetsolomonudofia@gmail.com>
2025-11-08 12:49:32 +0100

    models setup done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "description",
+        "status",
+        "user_id"
+    ];
+
+    public function users(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
